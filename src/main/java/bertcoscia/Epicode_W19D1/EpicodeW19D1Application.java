9047da4ec1bb8952316e27a87ba5b88ecd7db442
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-09-16 16:26:56 +0200

    Initial commit

---
+++ src/main/java/bertcoscia/Epicode_W19D1/EpicodeW19D1Application.java
@@ -0,0 +1,13 @@
+package bertcoscia.Epicode_W19D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeW19D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeW19D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/bertcoscia/Epicode_W19D1/EpicodeW19D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bertcoscia.Epicode_W19D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeW19D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
